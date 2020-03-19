cefa91cbdd3bb35c133008a153865a3d6bb2d660
cjorosco <58345738+cjorosco@users.noreply.github.com>
2020-03-19 11:47:38 -0500

    Add files via upload

---
+++ DSC530_week2_corosco.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 09:37:35 2020
+
+@author: corosco
+
+Display the text “Hello World! I wonder why that is always the default coding text to start with”
+Add two numbers together
+Subtract a number from another number
+Multiply two numbers
+Divide between two numbers
+Concatenate two strings together (any words)
+Create a list of 4 items (can be strings, numbers, both)
+Append an item to your list (again, can be a string, number)
+Create a tuple with 4 items (can be strings, numbers, both)
+
+"""
+
+print('Hello World! I wonder why that is always the default coding text to start with')
+new_nbr = 3+4
+print(new_nbr)
+new_nbr = 4-3
+print(new_nbr)
+new_nbr = 3*4
+print(new_nbr)
+new_nbr = 12/3
+print(new_nbr)
+new_string = "I get to work" + "from home"
+print(new_string)
+my_list = ["years married", "hawaii", 1977, 43]
+print(my_list)
+my_list.append("Wahiawa")
+print(my_list)
+my_tuple = ("Tuesday", "Wednesday", 2, 3)
+print(my_tuple)
